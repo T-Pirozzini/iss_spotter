b8c1d5cccd56ab5169b88c990993b8915072860d
vagrant <tpirozzini@gmail.com>
2022-02-18 04:10:42 +0000

    add spotter complete

--- index.js
+++ index.js
@@ -1,18 +1,46 @@
-const { fetchMyIP, fetchCoordsByIP } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
 
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds!`);
+  }
+};
 
-fetchMyIP((error, ip) => {  
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log("It didn't work!" , error);
-    return;
+    return console.log("It didn't work!", error);
   }
-  console.log('It worked! Returned IP:' , ip);
+  // success, print out the deets!
+  printPassTimes(passTimes);
 });
 
-fetchCoordsByIP('2001:569:bcea:e600:d2af:81c1:5e5:457e', (error, coordinates) => {
-  if (error){
-    console.log("It didn't work!", error);
-    return;
-  }
-  console.log('It worked! Returned coordinates:', coordinates);
-});
+// fetchMyIP((error, ip) => {  
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned IP:' , ip);
+// });
+
+
+// fetchCoordsByIP('2001:569:bcea:e600:d2af:81c1:5e5:457e', (error, coordinates) => {
+//   if (error){
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+//   console.log('It worked! Returned coordinates:', coordinates);
+// });
+
+
+// const exampleCoords = { latitude: '49.2114', longitude: '-124.0201' };
+
+// fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
+//   if (error) {
+//     console.log("It didn't work!" , error);
+//     return;
+//   }
+//   console.log('It worked! Returned flyover times:' , passTimes);
+// });
